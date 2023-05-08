61b8eff452718abbb4a6b4dd2f6a9dc16bf8dedf
Finn Wu <wlo1561411@gmail.com>
2023-05-09 00:51:14 +0800

    Add format function

--- LocalFormat/ViewController.swift
+++ LocalFormat/ViewController.swift
@@ -28,5 +28,81 @@ class ViewController: NSViewController {
 
         pathLabel.stringValue = savePath
     }
+
+    @IBAction func AddPath(_ sender: NSButton) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowedFileTypes = ["public.folder"]
+        openPanel.showsResizeIndicator = true
+        openPanel.showsHiddenFiles = false
+        openPanel.canChooseFiles = false
+        openPanel.canChooseDirectories = true
+        openPanel.allowsMultipleSelection = false
+        openPanel.canCreateDirectories = false
+        
+        openPanel.begin { [unowned self] (result) in
+            if result == NSApplication.ModalResponse.OK {
+                let directoryPath = openPanel.url?.path
+                if self.hasGitFolder(in: directoryPath) {
+                    UserDefaults.standard.set(directoryPath, forKey: "path")
+                    self.pathLabel.stringValue = savePath
+                }
+                else {
+                    self.showAlert(title: "錯誤", message: "選擇的目錄沒有包含 .git 文件夾。")
+                }
+            }
+        }
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = NSAlert()
+        alert.messageText = title
+        alert.informativeText = message
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
+    }
+    
+    private func hasGitFolder(in path: String?) -> Bool {
+        guard let path = path else { return false }
+        let fileManager = FileManager.default
+        let gitFolderPath = (path as NSString).appendingPathComponent(".git")
+        return fileManager.fileExists(atPath: gitFolderPath)
+    }
+    
+    @IBAction func foramt(_ sender: NSButton) {
+        findChangedSwiftFiles()
+    }
+    
+    private func findChangedSwiftFiles() {
+        let task = Process()
+        task.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+        task.currentDirectoryURL = URL(fileURLWithPath: savePath)
+        task.arguments = ["sh", "-c", """
+            if [ -f "./format.sh" ]; then
+                chmod +x ./format.sh
+                ./format.sh
+            else
+                echo "format.sh not found in the current directory."
+            fi
+        """]
+
+        let outputPipe = Pipe()
+        task.standardOutput = outputPipe
+        task.standardError = outputPipe
+
+        do {
+            try task.run()
+            task.waitUntilExit()
+
+            let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+            if let outputString = String(data: outputData, encoding: .utf8) {
+                print("Output:\n\(outputString)")
+            } else {
+                print("Failed to read output")
+            }
+        } catch {
+            print("Error running task: \(error)")
+        }
+    }
 }
 
---
+++ format.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 使用git status進行查找
+git status --porcelain | while read -r line; do
+  # 檢查當前行是否包含.swift文件
+  if echo "$line" | grep -q "\.swift"; then
+    # 獲取文件名
+    filename=$(echo "$line" | awk '{print $2}')
+    # 檢查文件是否有變更
+    if echo "$line" | grep -q -E "^(M|MM|A|AM|R|RM|D|MD)[[:space:]]"; then
+      echo "Changed Swift file: $filename"
+    fi
+  fi
+done
